806dc6550f7ade82e10d2c35474a6901659dcc1b
YouWuCN <william.wuy.cn@gmail.com>
2019-10-10 23:39:57 +0200

    close

--- Diagnostics/myDiagnostics.r
+++ Diagnostics/myDiagnostics.r
@@ -1,21 +1,59 @@
 
 
 myDiagnose <- function(myNet,cases){
-    results <- matrix(nrow = 10, ncol = 4)
-    rownames(results) <- 1:10
-    colnames(results) <- c('Pn','TB','LC','Br')
-    
-    #init: Pn=0,TB=0,LC=0,Br=0
-    obs = cases[1,]
-    obs[,c('Pn','TB','LC','Br')]=0
-    PPn = myNet$Pn[1,obs$Pn]
-    PTe = dnorm(obs$Te,myNet$Te_Pn[obs$Pn,1],myNet$Te_Pn[obs$Pn,2])
-    PTB = myNet$TB_VTB[(obs$VTB),obs$TB]
-    PLC = myNet$LC_Sm[(obs$Sm),obs$PLC]
-    PBr = myNet$Br_Sm[(obs$Sm),obs$Br]
-    PXR = myNet$XR_PnTBLC[obs$Pn,obs$TB,LC]
-    
-    
-    return(list('a','b'))
+    for(n in seq(10)){
+        obs = cases[n,]
+        obs[,c('Pn','TB','LC','Br')]=0
+        results <- c(0,0,0,0)
+        prob = getprob(obs, myNet)
+        randnum = runif(4000, 0, 1)
+        for(i in seq(1000)){
+            target = c('Pn','TB','LC','Br')
+            while(length(target) > 0){
+                cur = sample(target, 1, prob = rep(1, length(target)))
+                target <- target[target != cur]
+                proposal = obs
+                proposal[cur] = (proposal[cur] + 1) %% 2
+                
+                new_prob =  getprob(proposal, myNet)
+                if(new_prob >= prob){
+                    prob = new_prob
+                    obs = proposal
+                }else{
+                    if(randnum[1] > new_prob/prob){
+                        obs = proposal
+                        prob = new_prob
+                        randnum = randnum[-1]
+                    }
+                }
+            }
+            if(i>100){
+                results <- results+obs[,c('Pn','TB','LC','Br')]
+            }
+        }
+        results = results/900
+        cases[n, c('Pn','TB','LC','Br')] = results
+    }
+    return(cases[,c('Pn','TB','LC','Br')])
 }
+
+getprob <- function(obs, myNet){
+    
+    PPn = myNet$Pn[1,obs$Pn+1]
+    PTe = dnorm(obs$Te,myNet$Te_Pn[obs$Pn+1,1],myNet$Te_Pn[obs$Pn+1,2])
+    PTB = myNet$TB_VTB[(obs$VTB+1),obs$TB+1]
+    PLC = myNet$LC_Sm[(obs$Sm+1),obs$LC+1]
+    PBr = myNet$Br_Sm[(obs$Sm+1),obs$Br+1]
+    XR_line = (obs$Pn)*4+(obs$TB)*2+(obs$LC)*1
+    PXR = myNet$XR_PnTBLC[XR_line+1,obs$XR+1]
+    Dy_line = (obs$TB)*2+(obs$LC)*1
+    PDy = myNet$Dy_BrLC[Dy_line+1,obs$Dy+1] 
+    prob = PPn*PTe*PLC*PBr*PXR*PDy
+    
+    return(prob)
+}
+
+
+
+
 
